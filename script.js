43a1d854e695bbd88944676c4b8d3e23731dec8f
um-umbrella <97677833+um-umbrella@users.noreply.github.com>
2024-08-17 16:29:41 +0900

    CSSを追加

--- script.js
+++ script.js
@@ -3,30 +3,41 @@
 //,{ name: '', url: '', etc: '' }
 
 //◆作った
-const worksLists = [{ name: '習作', url: 'works/unknownPeople.html', etc: 'たいへんだった' }];
+const worksLists = [{ name: '習作', url: 'works/unknownPeople.html', etc: 'クリックで反応を返す。たいへんだった……' }];
 
 //◆メモ
 const memo = [
-  'パッケージを使うには（Node.jpが必要？）',
-  'Node.jpって結局何？',
-  'デスクトップ（GUI）アプリは作れる？ElectronやNW.js（Node.jpとも関わり）らしいが…',
+  '増えたテキストがフォーカス付きでだんだん下に伸びていくとか。ボタン画面と２カラムに分けたらゲームっぽくなる？',
+  'html2canvasリベンジ',
+  'JSでデスクトップ（GUI）アプリ制作もできるが、ElectronやNW.js（Node.jpとも関わり）が必要。ついでに、パッケージを使うにはNode.jpが必要？',
   'ブラウザいっぱいの宇宙作りたい',
-  'p5.jsなどを専用エディタ以外で使うには？jsファイルみたいに読み込むのかな。',
-  'この辺をやるよりも、CSSの基礎を先にしたほうがいい気がする。',
-  'キー入力でADWSを取得したら割と動くものが作れるのか…？'
+  'p5.jsなどを専用エディタ以外で使うには？jsファイルみたいに読み込むのかな。→どうやらそうらしい',
+  'JSの先をやるよりCSSの基礎を先にしたほうがいい気がする。',
+  'キー入力でADWSを取得したら割と動くものが作れるのか…？',
+  '└position:relative;で（ゲーム向けではないが）それっぽい動きはできるかも。'
 ];
 //◆メモ
 const data = [
   {
     name: 'MDN',
     url: 'https://developer.mozilla.org/ja/docs/Web',
-    etc: '和訳ままゆえ難解だが、最も情報が集まっている（ように見える）。'
+    etc: '最も情報が集まっている（ように見える）。たまに未翻訳で泣くが、読めないことはない。初見の学習よりは、ある程度分かってから見たほうがいい。'
   },
-  { name: 'CSS Stock', url: 'https://pote-chil.com/css-stock/ja', etc: 'CSSコピペの宝庫。いつか使うかも。' },
   {
-    name: 'CSSプロパティ早見表｜文系大学生のためのHTML／CSS入門',
+    name: 'Zenn',
+    url: 'https://zenn.dev/',
+    etc: 'エンジニアのための情報共有コミュニティ。いい記事がいくつかあった。当然ながら初心者向けではなさそう。読みやすいやつもある。'
+  },
+  { name: 'CSS Stock', url: 'https://pote-chil.com/css-stock/ja', etc: 'CSSコピペの宝庫。解体すれば勉強にもなる。' },
+  {
+    name: 'CSSプロパティ早見表',
     url: 'https://zenn.dev/ojk/books/intro-to-html-css/viewer/css-properties',
-    etc: '基本的なCSSまとめ'
+    etc: '『文系大学生のためのHTML／CSS入門』より。基本的なCSSまとめ。'
+  },
+  {
+    name: 'JavaScript Primer',
+    url: 'https://jsprimer.net/intro/preparation/',
+    etc: '情報量。しっかり度はMDNに近い。流しで読んでみるのがよさそうだが、初心者向けではない。'
   }
 ];
 
@@ -35,12 +46,17 @@ const do_its = [
   {
     name: '文系学生のための○○',
     url: 'https://zenn.dev/ojk?tab=books',
-    etc: 'JS、p5.js、HTML/CSS。基礎から順を追う学習。内容自体は初心者向けだが、見た内では最も新しい情報が自然と入れられている。最初に触るのにいい。'
+    etc: 'JS、p5.js、HTML/CSS。基礎から順を追う学習。内容自体は初心者向けだが、新しい情報が自然と入れられている。教科書にいい。'
   },
   {
     name: 'VScodeでGithub（の超基本）',
     url: 'https://zenn.dev/ojk/books/github-vscode',
-    etc: 'このサイトを公開するために「Githubでウェブサイトを公開する」まで読んだ。後は読んでない。'
+    etc: '「Githubでウェブサイトを公開する」まで読んだ。後は読まなくていっか……。'
+  },
+  {
+    name: 'CODEPREP',
+    url: 'https://codeprep.jp/',
+    etc: 'プログラミング学習サイト。完成形を見られるもののJSは内容が古い。鵜呑みにしないほうがよさそう。'
   }
 ];
 
@@ -51,64 +67,70 @@ const dont_its = [
     url: 'https://ja.javascript.info/',
     etc: 'JS学習サイト。よさそう。むずそう。'
   },
-  { name: 'html2canvas', url: 'https://html2canvas.hertzen.com/', etc: 'スクショ撮るやつ。いつかやりたい' },
+  { name: 'html2canvas', url: 'https://html2canvas.hertzen.com/', etc: 'スクショ撮るやつ。いつかやりたい。' },
   {
-    name: 'CODEPREP',
-    url: 'https://codeprep.jp/',
-    etc: 'プログラミング学習サイト。こういう系の中で一番「勉強」はしやすい。やってない。'
-  },
-  { name: 'chot.design', url: 'https://chot.design/', etc: 'デザイン学習サイト。いつか見ようと思って全然見てない。' },
-  {
-    name: '最低限の知識で簡単なWebアプリを作る（生JavaScript vs React&TypeScript、やりたいこと別サンプル集）',
+    name: '最低限の知識で簡単なWebアプリを作る',
     url: 'https://qiita.com/birdwatcher/items/db81e4ef637d29d7d193',
-    etc: '広めにいろいろ乗ってる。コード、TypeScriopt……一回読むとよさそう。'
+    etc: '生JavaScript vs React&TypeScript、やりたいこと別サンプル集。広めにいろいろ載ってる。コード、TypeScriopt。一回読むとよさそう。'
   },
   {
-    name: 'JavaScript Primer',
-    url: 'https://jsprimer.net/intro/preparation/',
-    etc: '情報量。しっかり度はMDNに近い。流しで読んでみるのがよさそう'
-  },
-  {
-    name: '【Electron】HTML/CSS/JavaScript でデスクトップアプリを作った【インストールから exe 化まで】',
+    name: 'HTML/CSS/JavaScript でデスクトップアプリを作った',
     url: 'https://qiita.com/ndj/items/a07b433aa3f6f6e8742f',
-    etc: 'タイトル通り。ふ～～ん。分かりやすい。'
+    etc: 'Electronのインストールからexe化まで。タイトル通り。ふ～～ん。分かりやすいかも。自分用の何かは作れそう。'
   },
   {
     name: 'ようこそ！Electron入門',
     url: 'https://qiita.com/umamichi/items/6ce4f46c1458e89c4cfc',
-    etc: '上と関連して。分かりやすそう。ただ記事自体がかなり古いし、Electron自体セキュリティの問題に発展するらしい。初心者には怖い。'
+    etc: '上と関連して。分かりやすいが記事自体が古い。Electron自体セキュリティの問題もあるらしい。初心者には怖い。'
   },
   {
     name: 'あなたのWebサイトをたった5分でiOS/Androidアプリにする方法',
     url: 'https://zenn.dev/rdlabo/articles/4a241cacc7e364be8066',
     etc: '分かりやすそう。何をするにもNode.jpは必要らしい。あとandroidなんとか。'
   },
-  { name: 'Zenn', url: 'https://zenn.dev/', etc: 'エンジニアのための情報共有コミュニティ。いい記事がいくつかあった。' },
   {
     name: '令和のHTML/CSS/JavaScriptの書き方50選',
     url: 'https://zenn.dev/necscat/articles/bc9bba54babaf5',
     etc: '便利一覧。情報が新しくてよい。'
   },
-  { name: '', url: 'https://zenn.dev/dadada/articles/8faf8e1e20c0a5', etc: '理解しやすそう。' },
   {
-    name: '',
+    name: '【全30項目】コーディング時のルールや思想（HTML/CSS/Sass/JSなど）',
+    url: 'https://zenn.dev/dadada/articles/8faf8e1e20c0a5',
+    etc: '理解しやすそう。'
+  },
+  {
+    name: 'javascriptから別のjavascriptの処理を呼出し実行する',
     url: 'https://qiita.com/ydzum1123/items/afcb31236502997dc19d',
     etc: '外部JSの呼び出しができる？その先がどうかは分からないが'
+  },
+  {
+    name: '純粋な JavaScript を使ったブロック崩しゲーム',
+    url: 'https://developer.mozilla.org/ja/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript',
+    etc: '信頼のMWD。チュートリアル。'
+  }
+];
+
+const no_touch = [
+  {
+    name: 'chot.design',
+    url: 'https://chot.design/',
+    etc: 'デザイン学習サイト。最終更新が古い。リンク切れまであるので見なくていいかも。'
   }
 ];
 
 //関数の形で四回分呼ぶ
 listCreate(['works', worksLists]);
-memoList();
+memoList(['memo', memo]);
 listCreate(['data', data]);
 listCreate(['do_it', do_its]);
 listCreate(['dont_it', dont_its]);
+listCreate(['no_touch', no_touch]);
 
-//配列表示（使い回しはできない…）
-function memoList() {
-  const list = document.getElementById('memo');
+//配列を表示
+function memoList(e) {
+  const list = document.getElementById(e[0]);
   const ol = document.createElement('ol');
-  for (const txt of memo) {
+  for (const txt of e[1]) {
     const li = document.createElement('li');
     li.textContent = txt;
 
@@ -116,6 +138,35 @@ function memoList() {
     ol.appendChild(li);
   }
 }
+
+//リンク・リスト
+//リストの中にdlを入れたら見た目が整う…？
+function listCreate(e) {
+  const nowList = document.getElementById(e[0]);
+  for (const prop of e[1]) {
+    //listを生成する
+
+    const dl = document.createElement('dl');
+    const dt = document.createElement('dt');
+    const a = document.createElement('a');
+    const dd = document.createElement('dd');
+
+    //オブジェクトの内容をリスト内容に入れる
+    a.textContent = prop.name;
+    a.href = prop['url'];
+    dd.textContent = prop['etc'];
+
+    //dtとddは独立していないといけない
+    nowList.appendChild(dl);
+
+    //    nowList.appendChild(dl);
+    dl.appendChild(dt);
+    dt.appendChild(a);
+    dt.parentElement.appendChild(dd);
+  }
+}
+
+/* バックアップ
 
 //リンク・リスト
 function listCreate(e) {
@@ -139,3 +190,6 @@ function listCreate(e) {
     dt.parentElement.appendChild(dd);
   }
 }
+
+
+ */
