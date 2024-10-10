e66eba045ace715134504de502a59564b3ae0c5c
um-umbrella <97677833+um-umbrella@users.noreply.github.com>
2024-10-10 22:58:20 +0900

    メモ欄の追加とCSS調整

--- works/journal_support/script.js
+++ works/journal_support/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const version = 'ver 1.5';
+const version = 'ver 1.6';
 const theme = ['mint', 'red', 'grass', 'sea', 'robot', 'brown', 'gray', 'night'];
 
 const h1 = document.querySelector('h1');
