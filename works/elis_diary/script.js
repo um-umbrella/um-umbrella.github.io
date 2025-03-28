685ae4dd0534da15ed3f180642b4b4565f3e0c8b
um-umbrella <97677833+um-umbrella@users.noreply.github.com>
2025-03-28 17:23:40 +0900

    CSS側追加

--- works/elis_diary/script.js
+++ works/elis_diary/script.js
@@ -4,9 +4,8 @@
 //疑似パスワード
 
 window.onload = function () {
-    document.body.style.display = 'none';
     let pass = prompt('にんじんの次に苦いものといえば？');
     if (pass === 'fj') {
-        document.body.style.display = 'block';
+        document.getElementById('loading').classList.add('loaded');
     }
 };
